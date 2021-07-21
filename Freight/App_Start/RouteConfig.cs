3a0dd977a51a27c586e1ed24cb297c3bca9f3f7d
Michael Pham <phamminh1309@gmail.com>
2021-07-22 00:04:44 +0700

    init

---
+++ Freight/App_Start/RouteConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Freught1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Upload rate",
+               url: "uploadrate",
+               defaults: new { controller = "UploadRate", action = "Index", id = UrlParameter.Optional },
+           namespaces: new[] { "Freught1.Controllers" }
+           );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+           // routes.MapRoute(
+           //    name: "Default",
+           //    url: "{controller}/{action}/{id}",
+           //    defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+           //);
+            routes.MapRoute(
+                name: "Calculator",
+                url: "calculator",
+                defaults: new { controller = "Calculator", action = "Index", id = UrlParameter.Optional },
+            namespaces: new[] { "Freught1.Controllers" }
+            );
+            routes.MapRoute(
+               name: "UploadRate",
+               url: "uploadRate",
+               defaults: new { controller = "UploadRate", action = "Index", id = UrlParameter.Optional },
+           namespaces: new[] { "Freught1.Controllers" }
+           );
+        }
+    }
+}
