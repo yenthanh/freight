87c722f104252822395a75a5bc1cf73cab8c9b45
Michael Pham <michael@sapotacorp.vn>
2021-09-14 08:06:19 +0300

    Completed API Export file Excel

--- Freight/Controllers/CalculatorController.cs
+++ Freight/Controllers/CalculatorController.cs
@@ -5,12 +5,10 @@ using ServiceDB.Service;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Data;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Excel = Microsoft.Office.Interop.Excel;
 using OfficeOpenXml;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace Freught1.Controllers
 {
@@ -69,7 +67,6 @@ namespace Freught1.Controllers
                 string logMsg = CarrierManager.Instance.Log;
                 var x = new { LogMsg = logMsg, Data = result };
                 //label6.Text = CarrierManager.Instance.Log;
-                ViewBag.List = result;
                 return Json(new JsonObject(0, "SUCCESS", x), JsonRequestBehavior.AllowGet);
             }
             catch (Exception ex)
@@ -78,55 +75,44 @@ namespace Freught1.Controllers
             }
 
         }
-  
-        public ActionResult Export()
+      
+        public ActionResult Export(string from, string to, string service, string packageType, string weight, string region)
         {
             string[] title = new string[] { "Carrier", "Service Type", "Packaging Type", "Weight Range(kg)", "Current FSC", "Working Days(day)", "Rate Before Surcharge (SGD)", "Rate After Surcharge (SGD)" };
-         
             var stream = new MemoryStream();
+            var result = CarrierManager.Instance.GetCalculatorPrice( from,to,service,packageType, weight == "" ? 0 : float.Parse(weight),  region);
             using (var package = new ExcelPackage(stream))
             {
                 var worksheet = package.Workbook.Worksheets.Add("Sheet1");
-                    worksheet.Cells[1, 1].LoadFromText("Carrier");
-                    worksheet.Cells[1, 2].LoadFromText("Service Type");
-                    worksheet.Cells[1, 3].LoadFromText("Packaging Type");
-                    worksheet.Cells[1, 4].LoadFromText("Weight Range(kg)");
-                    worksheet.Cells[1, 5].LoadFromText("Current FSC");
-                    worksheet.Cells[1, 6].LoadFromText("Working Days(day)");
-                    worksheet.Cells[1, 7].LoadFromText("Rate Before Surcharge (SGD)");
-                    worksheet.Cells[1, 8].LoadFromText("Rate After Surcharge (SGD)");
-                for(int row = 2;row <=12; row=row+2)
+                worksheet.Cells[1, 1].LoadFromText("Carrier");
+                worksheet.Cells[1, 2].LoadFromText("Service Type");
+                worksheet.Cells[1, 3].LoadFromText("Packaging Type");
+                worksheet.Cells[1, 4].LoadFromText("Weight Range(kg)");
+                worksheet.Cells[1, 5].LoadFromText("Current FSC");
+                worksheet.Cells[1, 6].LoadFromText("Working Days(day)");
+                worksheet.Cells[1, 7].LoadFromText("Rate Before Surcharge (SGD)");
+                worksheet.Cells[1, 8].LoadFromText("Rate After Surcharge (SGD)");
+                int row = 2;
+                foreach(var index in result)
                 {
-                    worksheet.Cells[row, 1].LoadFromText("FedEx");
-                    worksheet.Cells[row, 2].LoadFromText("FedEx International Priority Export Envelope");
-                    worksheet.Cells[row, 3].LoadFromText("ENVELOPE");
-                    worksheet.Cells[row, 4].LoadFromText("0.1-2.5");
-                    worksheet.Cells[row, 5].LoadFromText("0");
-                    worksheet.Cells[row, 6].LoadFromText("");
-                    worksheet.Cells[row, 7].LoadFromText("15.92");
-                    worksheet.Cells[row, 8].LoadFromText("15.92");
-                }
-                for (int row1 = 3; row1 < 12; row1 = row1 + 2)
-                {
-                    worksheet.Cells[row1, 1].LoadFromText("DHL");
-                    worksheet.Cells[row1, 2].LoadFromText("DHL Express International Export Zone For Multiper Above 30Kg");
-                    worksheet.Cells[row1, 3].LoadFromText("PACKAGE");
-                    worksheet.Cells[row1, 4].LoadFromText("30.1-500");
-                    worksheet.Cells[row1, 5].LoadFromText("0");
-                    worksheet.Cells[row1, 6].LoadFromText("");
-                    worksheet.Cells[row1, 7].LoadFromText("5.92");
-                    worksheet.Cells[row1, 8].LoadFromText("5.92");
-                }
+                    worksheet.Cells[row, 1].LoadFromText(index.CARRIER_NAME);
+                    worksheet.Cells[row, 2].LoadFromText(index.SERVICE_TYPE);
+                    worksheet.Cells[row, 3].LoadFromText(index.PACKAGE_TYPE);
+                    worksheet.Cells[row, 4].LoadFromText(index.WEIGHT_RANGE);
+                    worksheet.Cells[row, 5].LoadFromText(index.SURCHARGE.ToString());
+                    worksheet.Cells[row, 6].LoadFromText(index.WORKING_DAYS);
+                    worksheet.Cells[row, 7].LoadFromText(index.COST);
+                    worksheet.Cells[row, 8].LoadFromText((float.Parse(index.COST) * (1 + index.SURCHARGE / 100)).ToString());
+                    row++;
+                } 
 
                 package.Save();
             }
             stream.Position = 0;
-            string excelName = "Report.xlsx";
+            string excelName = "Report " + from + "-" + to + ".xlsx";
             return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
-           
-
-
         }
+
         public IList<PriceResultItem> GetPriceAdvanceList(CheckAdvePriceByWeight model)
         {
                 string serviceType = string.IsNullOrEmpty(model.ServiceType) ? "" : model.ServiceType;
